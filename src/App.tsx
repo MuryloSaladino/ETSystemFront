976bc5ffc489ab0f7dabed627fdea3fee37cb856
MuryloSaladino <murylosaladino@gmail.com>
2024-03-20 15:51:21 -0300

    feat: toastify styles

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,14 @@ import { ResetStyles } from "./styles/ResetStyles";
 import { ToastContainer } from "react-toastify";
 import 'react-toastify/dist/ReactToastify.css';
 
-import { RoutesMain } from "./routes/RoutesMain"
+import RoutesMain from "./routes/RoutesMain";
 
 function App() {
 
   return (
     <>
-      <GlobalStyles/>
       <ResetStyles/>
+      <GlobalStyles/>
 
       <RoutesMain/>
 

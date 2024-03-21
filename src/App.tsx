734c6da473ed3bc808c23427a75e2b4c41e80120
Murylo Saladino <murylosaladino@gmail.com>
2024-03-21 11:16:31 -0300

    refact: global styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
-import { GlobalStyles } from "./styles/GlobalStyles"
-import { ResetStyles } from "./styles/ResetStyles";
+// import { GlobalStyles } from "./styles/GlobalStyles"
+// import { ResetStyles } from "./styles/ResetStyles";
 
 import { ToastContainer } from "react-toastify";
 import 'react-toastify/dist/ReactToastify.css';
 
 import RoutesMain from "./routes/RoutesMain";
 
+import './styles/Reset.css'
+import './styles/GlobalStyles.css'
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
@@ -15,9 +17,6 @@ function App() {
 
   return (
     <>
-      <ResetStyles/>
-      <GlobalStyles/>
-
       <RoutesMain/>
 
       <ToastContainer/>

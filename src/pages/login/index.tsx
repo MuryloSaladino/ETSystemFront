962bc13f70c51cfe99f8fb27e35282ec09378d4c
Murylo Saladino <murylosaladino@gmail.com>
2024-03-21 21:26:35 -0300

    test: trying mui

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -3,9 +3,28 @@ import Stack from '@mui/material/Stack'
 import TextField from '@mui/material/TextField'
 import Typography from '@mui/material/Typography'
 import { Bosch } from '../../components'
+import { useState, ChangeEvent } from 'react'
 
+
+interface IInputs {
+    username: string;
+    password: string;
+}
 
 const Login = () => {
+
+    const [inputs, setInputs] = useState<IInputs>({
+        username: '',
+        password: ''
+    });
+
+    const handleChange = (e:ChangeEvent<HTMLInputElement>) => {
+        setInputs((prevState) => ({
+            ...prevState,
+            [e.target.name] : e.target.value 
+        }))
+    }
+
 
     return (
         <>
@@ -13,12 +32,22 @@ const Login = () => {
                 <Stack width="500px" maxWidth="90%" minHeight="40vh" direction="column" alignItems="center" gap="30px">
                     <Bosch/>
                     <Typography variant="h3">Fazer login</Typography>
-                    <Stack width="100%" gap="20px">
-                        <TextField label="Username" variant="outlined" required/>
-                        <TextField label="Password" variant="outlined" type="password" required/>
-                    </Stack>
-                    <Stack alignSelf="end">
-                        <Button size='large' variant="outlined">Login</Button>
+                    <Stack width="100%">
+                        <form style={{ width:"100%", display:"flex", flexDirection:"column", gap:"20px" }}>
+                            <TextField
+                                name="username"
+                                label="Username"
+                                variant="outlined"
+                                required
+                                onChange={handleChange}/>
+                            <TextField
+                                name="password"
+                                label="Password"
+                                variant="outlined"
+                                type="password" required
+                                onChange={handleChange}/>
+                            <Button size='large' variant="contained" sx={{alignSelf:"end"}}>Login</Button>
+                        </form>
                     </Stack>
                 </Stack>
             </Stack>

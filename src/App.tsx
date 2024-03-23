44b6c449f4adbebb53cb13392371dabaac62ace4
Murylo Saladino <murylosaladino@gmail.com>
2024-03-22 23:06:31 -0300

    remove: toastify

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,10 @@
-// import { GlobalStyles } from "./styles/GlobalStyles"
-// import { ResetStyles } from "./styles/ResetStyles";
-
-import { ToastContainer } from "react-toastify";
-import 'react-toastify/dist/ReactToastify.css';
-
 import RoutesMain from "./routes/RoutesMain";
 
 import './styles/Reset.css'
 import './styles/GlobalStyles.css'
-import '@fontsource/roboto/300.css';
-import '@fontsource/roboto/400.css';
-import '@fontsource/roboto/500.css';
-import '@fontsource/roboto/700.css';
 
 function App() {
-
-  return (
-    <>
-      <RoutesMain/>
-
-      <ToastContainer/>
-    </>
-  )
+  return <RoutesMain/>
 }
 
 export default App

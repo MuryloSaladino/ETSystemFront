3f3ee800c5c7f9dd60fed21cf759d31bc3ff5e4a
Murylo Saladino <murylosaladino@gmail.com>
2024-04-01 20:13:50 -0300

    fix: forwarding ref

--- src/components/SwitchInput/index.tsx
+++ src/components/SwitchInput/index.tsx
@@ -1,15 +1,15 @@
 import { TextField } from "@mui/material"
-import { useState } from "react"
+import { forwardRef, useState } from "react"
 
-const SwitchInput = () => {
+
+const SwitchInput = forwardRef(() => {
 
     const [edit, setEdit] = useState<boolean>(false)
 
     return(
-        <TextField 
+        <TextField
             disabled={!edit}
             onDoubleClick={() => setEdit((prev) => !prev)}/>
-    )
-}
+)})
 
 export default SwitchInput
--- src/pages/settings/index.tsx
+++ src/pages/settings/index.tsx
@@ -1,6 +1,5 @@
 import { Container, Divider, Typography } from "@mui/material"
-import { CustomAppBar } from "../../components"
-import EditableInfo from "./EditableInfo";
+import { CustomAppBar, SwitchInput } from "../../components"
 import { useContext } from "react"
 import { UserContext } from "../../context/UserContext"
 import { MessageContext } from "../../context/MessageContext";
@@ -17,12 +16,14 @@ const SettingsPage = () => {
 
     const submit = async (data:FieldValues) => {
         const token = localStorage.getItem("@TOKEN")
-        try {
-            await updateUser(user!.idUser, token!, clearEmptyProperties(data))
-            popNotification("Your data has been updated", "success")
-            buildUser()
-        } catch (error) {
-            popNotification("Oops! Something went wrong", "error")
+        if(user && token) {
+            try {
+                await updateUser(user.idUser, token, clearEmptyProperties(data))
+                popNotification("Your data has been updated", "success")
+                buildUser()
+            } catch (error) {
+                popNotification("Oops! Something went wrong", "error")
+            }
         }
     }
 
@@ -33,35 +34,22 @@ const SettingsPage = () => {
             <Container maxWidth="md">
                 <Typography variant="h4">Informações Básicas</Typography>
 
-                {
-                    user &&
-                    <form onSubmit={handleSubmit((data) => submit(data))}>
-                        <EditableInfo
-                            nameProp={"username"} valueProp={user.username}
-                            useFormRegister={register} useFormSetValue={setValue}
-                            pattern={/^[a-zA-Z][a-zA-Z0-9_]{3,}$/}/>
-                        <Divider/>
-                        <EditableInfo
-                            nameProp={"name"} valueProp={user.name}
-                            useFormRegister={register} useFormSetValue={setValue}
-                            pattern={/^[a-zA-Z ]+$/}/>
-                        <Divider/>
-                        <EditableInfo
-                            nameProp={"email"} valueProp={user.email}
-                            useFormRegister={register} useFormSetValue={setValue}
-                            pattern={/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/}/>
-                        <Divider/>
-                        <EditableInfo
-                            nameProp={"contact"} valueProp={user.contact}
-                            useFormRegister={register} useFormSetValue={setValue}
-                            pattern={/^\(\d{2}\)\d{5}-\d{4}$/}/>
-                        <Divider/>
-                        <EditableInfo
-                            nameProp={"dateOfBirth"} valueProp={user.dateOfBirth}
-                            useFormRegister={register} useFormSetValue={setValue}
-                            pattern={/^(0[1-9]|[12][0-9]|3[01])\/(0[1-9]|1[0-2])\/\d{4}$/}/>
-                    </form>
-                }
+                <form onSubmit={handleSubmit((data) => submit(data))}>
+                    <SwitchInput 
+                        {...register("username", { pattern: /^[a-zA-Z][a-zA-Z0-9_]{3,}$/ })}/>
+                    <Divider/>
+                    <SwitchInput 
+                        {...register("name", { pattern: /^[a-zA-Z ]+$/ })}/>
+                    <Divider/>
+                    <SwitchInput 
+                        {...register("email", { pattern: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/ })}/>
+                    <Divider/>
+                    <SwitchInput 
+                        {...register("contact", { pattern: /^\(\d{2}\)\d{5}-\d{4}$/ })}/>
+                    <Divider/>
+                    <SwitchInput 
+                        {...register("dateOfBirth", { pattern: /^(0[1-9]|[12][0-9]|3[01])\/(0[1-9]|1[0-2])\/\d{4}$/ })}/>
+                </form>
             </Container>
         </>
     )

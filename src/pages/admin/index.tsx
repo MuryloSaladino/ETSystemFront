f25de101b00f5e020762dac49a4f0e397b9ee9e4
Murylo Saladino <murylosaladino@gmail.com>
2024-04-02 08:53:55 -0300

    feat: access rendering

--- src/pages/admin/index.tsx
+++ src/pages/admin/index.tsx
@@ -1,7 +1,9 @@
+import { adminDisciplinesAccess, adminInstitutionsAccess, adminInstructorsAccess, adminStudentsAccess, adminUsersAccess } from "../../interfaces";
 import { CustomAppBar, NavigationGrid } from "../../components"
 import { UserContext } from "../../context/UserContext";
 import { IAccess } from "../../interfaces";
 import { useContext, useEffect, useState } from "react";
+
 
 const AdminPage = () => {
 
@@ -10,7 +12,21 @@ const AdminPage = () => {
 
     useEffect(() => {
         const buildAccess = async () => {
-            
+            setNavigationItems([])
+            if(user && user.idAdministrator) {
+                setNavigationItems([
+                    adminDisciplinesAccess,
+                    adminInstitutionsAccess,
+                    adminUsersAccess
+                ])
+                if(user.institution.isBosch) {
+                    setNavigationItems((prev) => [
+                        ...prev,
+                        adminStudentsAccess,
+                        adminInstructorsAccess
+                    ])
+                }
+            }
         }
         buildAccess()
     }, [user])

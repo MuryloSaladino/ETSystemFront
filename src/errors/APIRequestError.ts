37b169d4370207bbe8c58f19f214ff92c99d6347
MuryloSaladino <murylosaladino@gmail.com>
2024-04-04 16:44:22 -0300

    feat: notification function

--- src/errors/APIRequestError.ts
+++ src/errors/APIRequestError.ts
@@ -1,6 +1,6 @@
 class APIRequestError extends Error {
 
-    private static messages:Record<number, string> = {
+    public static messages:Record<number, string> = {
         400: "Oops! Looks like something is wrong in your request.",
         401: "You are not allowed to do that.",
         403: "You do not have access for that",
--- src/utils/AppToast.ts
+++ src/utils/AppToast.ts
@@ -8,12 +8,25 @@ class AppToast {
         return localStorage.getItem("@THEME") === "DARK" ? "dark" : "light"
     }
 
-    static notify(message:string, type:IToastType = "default"):void {
+    public static notify(message:string, type:IToastType = "default"):void {
         toast(
             message, 
             { 
                 type: type,
-                theme: AppToast.getTheme(),
+                theme: AppToast.getTheme()
+            }
+        )
+    }
+
+    public static notifyError(error:APIRequestError | Error):void {
+        const notificationMessage = error instanceof APIRequestError ? 
+            error.message : APIRequestError.messages[400]
+            
+        toast(
+            notificationMessage, 
+            { 
+                type: "error",
+                theme: AppToast.getTheme()
             }
         )
     }
